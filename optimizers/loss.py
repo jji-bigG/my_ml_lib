ef5422daa0542e4f7a5e5be54d87460f436536a7
jerryji <rj378@cornell.edu>
2024-05-20 08:41:33 +0800

    defined more loss functions

--- optimizers/loss.py
+++ optimizers/loss.py
@@ -1,7 +1,21 @@
 import numpy as np
 
-# mse loss function
-
 
 def mse(y_true, y_pred):
     return np.mean((y_true - y_pred) ** 2)
+
+
+def mae(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
+
+def cross_entropy(y_true, y_pred):
+    return -np.mean(y_true * np.log(y_pred + 1e-8))
+
+
+def binary_crossentropy(y_true, y_pred):
+    return -np.mean(y_true * np.log(y_pred + 1e-8) + (1 - y_true) * np.log(1 - y_pred + 1e-8))
+
+
+def categorical_crossentropy(y_true, y_pred):
+    return -np.mean(np.sum(y_true * np.log(y_pred + 1e-8), axis=1))
