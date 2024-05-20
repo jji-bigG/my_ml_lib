3c27df95e38a82fccc4fb673593e0ac860a57a04
jerryji <rj378@cornell.edu>
2024-05-20 13:51:31 +0800

    adaboost & testing suite
    - reorganized directory structure

---
+++ models/ensemble/adaboost.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from models.cart import DecisionTree
+
+
+class AdaBoost:
+    def __init__(self, n_estimators=50, max_depth=1):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.models = []
+        self.model_weights = []
+
+    def fit(self, X, y):
+        n_samples = X.shape[0]
+        weights = np.ones(n_samples) / n_samples
+
+        for _ in range(self.n_estimators):
+            model = DecisionTree(max_depth=self.max_depth)
+            model.fit(X, y, sample_weight=weights)
+            predictions = model.predict(X)
+
+            misclassified = (predictions != y).astype(int)
+            error = np.dot(weights, misclassified) / np.sum(weights)
+
+            if error == 0:
+                alpha = np.inf
+            elif error == 1:
+                alpha = -np.inf
+            else:
+                alpha = 0.5 * np.log((1 - error) / error)
+
+            weights *= np.exp(alpha * misclassified)
+            weights /= np.sum(weights)
+
+            self.models.append(model)
+            self.model_weights.append(alpha)
+
+    def predict(self, X):
+        model_preds = np.array([model.predict(X) for model in self.models])
+        weighted_preds = np.dot(self.model_weights, model_preds)
+        return np.sign(weighted_preds)
+
+    def evaluate(self, X, y):
+        y_pred = self.predict(X)
+        return np.mean(y == y_pred)
---
+++ test/test_models/ensemble/test_adaboost.py
@@ -0,0 +1,56 @@
+import unittest
+import numpy as np
+
+from models.ensemble.adaboost import AdaBoost
+
+
+class TestAdaBoost(unittest.TestCase):
+    def setUp(self):
+        self.X = np.array([[1, 2], [1, -1], [2, 2], [2, -1], [3, 2], [3, -1]])
+        self.y = np.array([1, -1, 1, -1, 1, -1])
+        self.weights = np.array([1, 1, 1, 1, 1, 1])
+
+    def test_basic_functionality(self):
+        ada = AdaBoost(n_estimators=10, max_depth=1)
+        ada.fit(self.X, self.y)
+        predictions = ada.predict(self.X)
+        self.assertTrue(np.array_equal(predictions, self.y))
+
+    def test_weighted_samples(self):
+        ada = AdaBoost(n_estimators=10, max_depth=1)
+        weights = np.array([10, 1, 10, 1, 10, 1])  # High weight on class 1
+        ada.fit(self.X, self.y)
+        predictions = ada.predict(self.X)
+        self.assertTrue(np.array_equal(predictions, self.y))
+
+    def test_no_variability(self):
+        y_same = np.array([1, 1, 1, 1, 1, 1])
+        ada = AdaBoost(n_estimators=10, max_depth=1)
+        ada.fit(self.X, y_same)
+        predictions = ada.predict(self.X)
+        self.assertTrue(np.all(predictions == 1))
+
+    def test_all_same_inputs(self):
+        X_same = np.array([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
+        ada = AdaBoost(n_estimators=10, max_depth=1)
+        ada.fit(X_same, self.y)
+        predictions = ada.predict(X_same)
+        self.assertTrue(np.all(predictions == np.sign(np.mean(self.y))))
+
+    def test_evaluate_accuracy(self):
+        ada = AdaBoost(n_estimators=10, max_depth=1)
+        ada.fit(self.X, self.y)
+        accuracy = ada.evaluate(self.X, self.y)
+        self.assertEqual(accuracy, 1.0)
+
+    def test_with_extreme_weights(self):
+        ada = AdaBoost(n_estimators=10, max_depth=1)
+        weights_extreme = np.array(
+            [0.1, 1000, 0.1, 1000, 0.1, 1000])  # Avoid zero weights
+        ada.fit(self.X, self.y)
+        predictions = ada.predict(self.X)
+        self.assertTrue(np.array_equal(predictions, self.y))
+
+
+if __name__ == '__main__':
+    unittest.main()
