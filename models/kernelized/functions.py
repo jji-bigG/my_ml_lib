a949e08aeb9bc7b01e16a7bd6b5624c8a97c1fe4
jerryji <rj378@cornell.edu>
2024-05-21 10:01:21 +0800

    added some more kernel functions

--- models/kernelized/functions.py
+++ models/kernelized/functions.py
@@ -17,3 +17,19 @@ def gaussian_kernel(x, y, sigma=5.0):
 
 def rbf_kernel(x, y, gamma=0.1):
     return np.exp(-gamma * np.linalg.norm(x - y) ** 2)
+
+
+def sigmoid_kernel(x, y, gamma=0.1, r=0, d=3):
+    return np.tanh(gamma * np.dot(x, y) + r)
+
+
+def fisher_kernel(x, y, gamma=0.1, r=0, d=3):
+    return np.tanh(gamma * np.dot(x, y) + r) + np.tanh(gamma * np.dot(x, y) + r)**2
+
+
+def laplacian_kernel(x, y, gamma=0.1):
+    return np.exp(-gamma * np.linalg.norm(x - y, 1))
+
+
+def nngp_kernel(x, y, gamma=0.1):
+    return np.exp(-gamma * np.linalg.norm(x - y, 2))
